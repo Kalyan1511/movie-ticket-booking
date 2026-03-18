36dad9834f3a8d5354941b1682899a6f7cb4fead
Kalyan Reddy <annepallikalyanreddy@gmail.com>
2026-03-18 14:58:47 +0530

    deployment setup

--- slot_booking/settings.py
+++ slot_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lrw_&97m1-nxmiwdn!%u0orzs020+)#$h*fbp!4t6t_d^r!bzf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,7 +48,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
 ]
+
 
 ROOT_URLCONF = 'slot_booking.urls'
 
@@ -119,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
